9e8c1b9c084b64de873c63f250e8e0932a1ae641
mayukh-7 <mayukhmaity7@gmail.com>
2025-09-29 21:32:29 +0530

    Added basicreact , basicvite and made a custom react

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "./Youtube.jsx";
+function App(){
+    const username = "Mayukh Maity"
+    return(
+        <>
+            <h1>Hello this is my first react application through vite {2+2}</h1>
+            <h1>Hello this is my first react application through vite {username}</h1>
+            <Youtube/>
+        </>
+        
+    )
+}
+
+export default App;
